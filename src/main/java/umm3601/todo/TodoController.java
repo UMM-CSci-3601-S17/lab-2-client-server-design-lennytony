da455f48b4ce418c828512d5707747470fda38eb
Tony Song <songx823@morris.umn.edu>
2017-01-28 17:21:57 -0600

    Make `listTodos()` and `limitTodos()` Issue: #3

---
+++ src/main/java/umm3601/todo/TodoController.java
@@ -0,0 +1,46 @@
+package umm3601.todo;
+
+import com.google.gson.Gson;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+
+public class TodoController {
+    private Todo[] todos;
+
+    public TodoController() throws IOException {
+        Gson gson = new Gson();
+        FileReader reader = new FileReader("src/main/data/todos.json");
+        todos = gson.fromJson(reader, Todo[].class);
+    }
+
+    // List users
+    public Todo[] listTodos(Map<String, String[]> queryParams) {
+        Todo[] filteredTodos = todos;
+
+        // Filter limit if defined
+        if(queryParams.containsKey("limit")) {
+            // Get the value of the query
+            int limit = Integer.parseInt(queryParams.get("limit")[0]);
+            filteredTodos = limitTodos(filteredTodos, limit);
+        }
+
+        return filteredTodos;
+    }
+
+    public Todo[] limitTodos(Todo[] filteredTodos, int limit) {
+        return Arrays.copyOf(filteredTodos, limit);
+    }
+
+    // Filter users by age
+//    public Todo[] filterUsersByAge(Todo[] filteredUsers, int age) {
+//        return Arrays.stream(filteredUsers).filter(x -> x.age == age).toArray(User[]::new);
+//    }
+
+    // Get a single user
+//    public Todo getUser(String id) {
+//        return Arrays.stream(users).filter(x -> x._id.equals(id)).findFirst().orElse(null);
+//    }
+}
