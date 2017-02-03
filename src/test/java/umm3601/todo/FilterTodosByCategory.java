fa7c9294ba25778701a3ceffbe4031c6799ce601
Dogxx000 <scot0530@morris.umn.edu>
2017-02-02 19:26:42 -0600

    Add testing for owner and category filtering Issue #5

---
+++ src/test/java/umm3601/todo/FilterTodosByCategory.java
@@ -0,0 +1,33 @@
+package umm3601.todo;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class FilterTodosByCategory {
+    @Test
+    public void filterTodosByOwner() throws IOException {
+        TodoController todoController = new TodoController();
+        Todo[] allTodos = todoController.listTodos(new HashMap<>());
+        Todo[] filterSoftDTodos = todoController.filterTodosByCategory(allTodos, "software design");
+        assertEquals("Incorrect number todos for software design", 74, filterSoftDTodos.length);
+        Todo[] filterMcPheeTodos = todoController.filterTodosByCategory(allTodos, "McPhee");
+        assertEquals("Incorrect number of McPhees", 0, filterMcPheeTodos.length);
+    }
+
+    @Test
+    public void listTodosWithOwnerFilter() throws IOException {
+        TodoController todoController = new TodoController();
+        Map<String, String[]> queryParams = new HashMap<>();
+        queryParams.put("category", new String[] {"software design"});
+        Todo[] filterSoftDTodos = todoController.listTodos(queryParams);
+        assertEquals("Incorrect number todos for software design", 61, filterSoftDTodos.length);
+        queryParams.put("category", new String[] {"McPhee"});
+        Todo[] filterMcPheeTodos = todoController.listTodos(queryParams);
+        assertEquals("Incorrect number of McPhees", 0, filterMcPheeTodos.length);
+    }
+}
