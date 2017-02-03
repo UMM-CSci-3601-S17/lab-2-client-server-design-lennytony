94547c0396af7df60b6641f60791056e24874de5
Dogxx000 <scot0530@morris.umn.edu>
2017-02-02 19:42:25 -0600

    Fix tests and rename methods Issue #5

--- src/test/java/umm3601/todo/FilterTodosByCategory.java
+++ src/test/java/umm3601/todo/FilterTodosByCategory.java
@@ -10,7 +10,7 @@ import static junit.framework.TestCase.assertEquals;
 
 public class FilterTodosByCategory {
     @Test
-    public void filterTodosByOwner() throws IOException {
+    public void filterTodosByCategory() throws IOException {
         TodoController todoController = new TodoController();
         Todo[] allTodos = todoController.listTodos(new HashMap<>());
         Todo[] filterSoftDTodos = todoController.filterTodosByCategory(allTodos, "software design");
@@ -20,12 +20,12 @@ public class FilterTodosByCategory {
     }
 
     @Test
-    public void listTodosWithOwnerFilter() throws IOException {
+    public void listTodosWithCategoryFilter() throws IOException {
         TodoController todoController = new TodoController();
         Map<String, String[]> queryParams = new HashMap<>();
         queryParams.put("category", new String[] {"software design"});
         Todo[] filterSoftDTodos = todoController.listTodos(queryParams);
-        assertEquals("Incorrect number todos for software design", 61, filterSoftDTodos.length);
+        assertEquals("Incorrect number todos for software design", 74, filterSoftDTodos.length);
         queryParams.put("category", new String[] {"McPhee"});
         Todo[] filterMcPheeTodos = todoController.listTodos(queryParams);
         assertEquals("Incorrect number of McPhees", 0, filterMcPheeTodos.length);
